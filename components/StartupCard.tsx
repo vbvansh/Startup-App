d231b537cfc106bacda1da2e10f22216150fb4ea
vb2910 <vb291024@gmail.com>
2024-12-18 19:46:35 +0530

    Data fetched from DB on homepage

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -2,23 +2,25 @@ import { EyeIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Author, Startup } from "@/sanity/types";
 // Define or import the formatDate function
 const formatDate = (dateString: string) => {
   const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
   return new Date(dateString).toLocaleDateString(undefined, options);
 };
 
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
-    const {
-      _createdAt,
-      views,
-      author : {_id: authorId, name},
-      title,
-      category,
-      _id,
-      image,
-      description,
-    } = post;
+  const {
+    _createdAt,
+    views,
+    author,
+    title,
+    category,
+    _id,
+    image,
+    description,
+  } = post;
   
     return (
       <li className="startup-card group">
@@ -32,14 +34,14 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
   
         <div className="flex-between mt-5 gap-5">
           <div className="flex-1">
-            <Link href={`/user/${authorId}`}>
-              <p className="text-16-medium line-clamp-1">{name}</p>
+            <Link href={`/user/${author?._id}`}>
+              <p className="text-16-medium line-clamp-1">{author?.name}</p>
             </Link>
             <Link href={`/startup/${_id}`}>
               <h3 className="text-26-semibold line-clamp-1">{title}</h3>
             </Link>
           </div>
-          <Link href={`/user/${authorId}`}>
+          <Link href={`/user/${author?._id}`}>
             <Image
               src="https://via.placeholder.com/48"
               alt="placeholder"
